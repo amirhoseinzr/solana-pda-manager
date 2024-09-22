8f400678e71f5e1494c5641734c306d69c68bddb
amirhosein <zr.amirhosein@gmail.com>
2024-09-22 22:15:38 +0330

    pda crud

--- programs/pda-manager/src/lib.rs
+++ programs/pda-manager/src/lib.rs
@@ -1,27 +1,103 @@
 use anchor_lang::prelude::*;
 
-declare_id!("sV6jc9DFyyceTiDazEzr7jHYXvzfsktke9UmtXTPydi");
+// Your program Id will be added here when you enter "build" command
+declare_id!("");
 
 #[program]
-pub mod pda_manager {
+pub mod pdamanager {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_pda(
+        ctx: Context<Initializepda>,
+        id: u64,
+        merchant_name: String,
+        amount: u64,
+    ) -> Result<()> {
+        let pda_account = &mut ctx.accounts.pda_account;
+
+        pda_account.id = id;
+        pda_account.merchant_name = merchant_name;
+        pda_account.amount = amount;
+        pda_account.owner = *ctx.accounts.authority.key;
+
+        Ok(())
+    }
+
+    pub fn modify_pda(
+        ctx: Context<Modifypda>,
+        _id: u64,
+        merchant_name: String,
+        amount: u64,
+    ) -> Result<()> {
+        let pda_account = &mut ctx.accounts.pda_account;
+        pda_account.merchant_name = merchant_name;
+        pda_account.amount = amount;
+
+        Ok(())
+    }
+
+    pub fn delete_pda(_ctx: Context<Deletepda>, _id: u64) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(instruction)]
-pub struct Initialize {}
+#[instruction(id : u64)]
+pub struct Initializepda<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
 
-struct ConversationPda<'info> {
-    #[account (
-    seeds = [b"conversation_pda"],
-    bump
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 8 + 32+ (4 + 12)+ 8 + 1,
+        seeds = [b"pda", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
     )]
+    pub pda_account: Account<'info, pdaAccount>,
 
-
+    pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(id : u64)]
+pub struct Modifypda<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
 
+    #[account(
+        mut,
+        seeds = [b"pda", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub pda_account: Account<'info, pdaAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(id : u64)]
+pub struct Deletepda<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [b"pda", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub pda_account: Account<'info, pdaAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct pdaAccount {
+    pub id: u64,
+    pub owner: Pubkey,
+    pub merchant_name: String,
+    pub amount: u64,
+}
